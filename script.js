66e57ba9e8c16b7061a44e0ceb7749b191f738ca
Al3xander123 <96358474+Al3xander123@users.noreply.github.com>
2022-04-04 07:09:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(".principal").fadeIn(time);
 /*CLICK MENU PRINCIPAL*/
 $(".principal").click(function(){
 	$(".s_televisores,.s_equipos_de_sonido,.s_computadoras").fadeOut(time);
-	$(".aiwa,.toshiba,.samsung,.sony,.pionner,.baja,.media,.alta").css("background-color","white");
+	$(".tag").css("background-color","white");
 	$(".televisores,.equipos_de_sonido,.computadoras").slideToggle(time);
 });
 
@@ -19,7 +19,7 @@ $(".principal").click(function(){
 $(".televisores").click(function(){
 	$(".s_equipos_de_sonido,.s_computadoras").fadeOut(time);
 	$(".div_equipos_de_sonido,.div_computadoras").fadeOut(time);
-	$(".aiwa,.toshiba,.samsung").css("background-color","white");
+	$(".tag").css("background-color","white");
 		
 	$(".s_televisores").fadeIn(time);
 	$(".div_televisores").fadeIn(time);
@@ -29,7 +29,7 @@ $(".televisores").click(function(){
 $(".equipos_de_sonido").click(function(){
 	$(".s_televisores,.s_computadoras").fadeOut(time);
 	$(".div_televisores,.div_computadoras").fadeOut(time);
-	$(".aiwa,.sony,.pionner").css("background-color","white");
+	$(".tag").css("background-color","white");
 	
 	$(".s_equipos_de_sonido").fadeIn(time);
 	$(".div_equipos_de_sonido").fadeIn(time);
@@ -38,7 +38,7 @@ $(".equipos_de_sonido").click(function(){
 $(".computadoras").click(function(){
 	$(".s_televisores,.s_equipos_de_sonido").fadeOut(time);
 	$(".div_televisores,.div_equipos_de_sonido").fadeOut(time);
-	$(".baja,.media,.alta").css("background-color","white");
+	$(".tag").css("background-color","white");
 	
 	$(".s_computadoras").fadeIn(time);
 	$(".div_computadoras").fadeIn(time);
@@ -46,49 +46,49 @@ $(".computadoras").click(function(){
 
 /*SELECCIONANDO UNA MARCA DE TV*/
 $(".li_tv_aiwa").click(function(){
-$(".toshiba,.samsung").css("background-color","white");
+$(".tag_tv").css("background-color","white");
 $(".aiwa").css("background-color","pink");
 });
 
 $(".li_tv_toshiba").click(function(){
-$(".aiwa,.samsung").css("background-color","white");
+$(".tag_tv").css("background-color","white");
 $(".toshiba").css("background-color","pink");
 });
 
 $(".li_tv_samsung").click(function(){
-$(".aiwa,.toshiba").css("background-color","white");
+$(".tag_tv").css("background-color","white");
 $(".samsung").css("background-color","pink");
 });
 
 /*SELECCIONANDO UNA MARCA EQUIPO DE SONIDO*/
 $(".li_equipo_aiwa").click(function(){
-$(".sony,.pionner").css("background-color","white");
+$(".tag_sonido").css("background-color","white");
 $(".aiwa").css("background-color","pink");
 });
 
 $(".li_equipo_sony").click(function(){
-$(".aiwa,.pionner").css("background-color","white");
+$(".tag_sonido").css("background-color","white");
 $(".sony").css("background-color","pink");
 });
 
 $(".li_equipo_pionner").click(function(){
-$(".aiwa,.sony").css("background-color","white");
+$(".tag_sonido").css("background-color","white");
 $(".pionner").css("background-color","pink");
 });
 
 /*SELECCIONANDO UNA MARCA DE COMPUTADORA*/
 $(".li_computadora_gbaja").click(function(){
-$(".media,.alta").css("background-color","white");
+$(".tag_cpu").css("background-color","white");
 $(".baja").css("background-color","pink");
 });
 
 $(".li_computadora_gmedia").click(function(){
-$(".baja,.alta").css("background-color","white");
+$(".tag_cpu").css("background-color","white");
 $(".media").css("background-color","pink");
 });
 
 $(".li_computadora_galta").click(function(){
-$(".baja,.media").css("background-color","white");
+$(".tag_cpu").css("background-color","white");
 $(".alta").css("background-color","pink");
 });
 
